3a5bb2ccbd0860c95284c44ce3e426c3086a96f0
Vagini <vaginipy97@gmail.com>
2024-11-19 10:29:19 +0530

    Initial commit

---
+++ src/main/java/com/visitor/petitefrance/PetitefranceApplication.java
@@ -0,0 +1,13 @@
+package com.visitor.petitefrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetitefranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetitefranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visitor/petitefrance/PetitefranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visitor.petitefrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetitefranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
